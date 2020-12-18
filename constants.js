ce9f9d1e5ced93660b39d669e576cb3d6fa6acd7
Redaa <164438@supinfo.com>
2020-12-18 15:03:06 +0100

    changed database url

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
 module.exports = {
-  MONGO_URI: "mongodb://localhost:27017/betweenus",
+  //MONGO_URI: "mongodb://localhost:27017/betweenus",
+  MONGO_URI:
+    "mongodb+srv://api_user:dRN4uN1Py8eXB2sc@cluster0.afxqv.mongodb.net/betweenus",
 };
